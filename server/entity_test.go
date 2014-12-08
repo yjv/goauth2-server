6dd14584d4b2429c2e9682090f3c28edb9e49880
Yosef Deray <yosefderay770@gmail.com>
2014-12-07 22:20:01 -0500

    added unit tests for the entity structs

---
+++ server/entity_test.go
@@ -0,0 +1,43 @@
+package server
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBasicOauthSessionRequest(t *testing.T) {
+
+	grant := "grant"
+	data := make(map[string]string)
+	data["key"] = "value"
+	data["key1"] = "value1"
+	request := NewBasicOauthSessionRequest(grant, data)
+
+	assert.Equal(t, grant, request.Grant())
+
+	value1, _ := request.Get("key")
+
+	assert.Equal(t, data["key"], value1)
+
+	value2, ok := request.Get("key1")
+
+	assert.Equal(t, data["key1"], value2)
+
+	_, ok = request.Get("key2")
+
+	assert.False(t, ok)
+}
+
+func TestOwnerFromClient(t *testing.T) {
+
+	client := &Client{
+		"id",
+		"name",
+		"redirectUri",
+	}
+
+	assert.Equal(t, &Owner{
+		"id",
+		"name",
+	}, OwnerFromClient(client))
+}
